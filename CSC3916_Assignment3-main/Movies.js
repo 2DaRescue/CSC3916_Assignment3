c395f1c212681007b824b9873719b8e811b70d2e
2DaRescue <d.san01@hotmail.com>
2025-03-16 19:11:38 -0600

    minor edits

--- CSC3916_Assignment3-main/Movies.js
+++ CSC3916_Assignment3-main/Movies.js
@@ -29,7 +29,7 @@ const MovieSchema = new mongoose.Schema({
     actorName: String,
     characterName: String,
   }],
-  imageURL: { type: String, default: "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" }, // ✅ New field for movie posters
+  imageURL: { type: String }, // ✅ New field for movie posters
 });
 
 
